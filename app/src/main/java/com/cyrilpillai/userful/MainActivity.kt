c68ba2d695c1ab934955f40d75de22ec2f9a31bc
cyrilpillai <cyrilpillai@gmail.com>
2018-04-30 00:54:25 +0530

    Dependencies added

--- app/src/main/java/com/cyrilpillai/userful/MainActivity.kt
+++ app/src/main/java/com/cyrilpillai/userful/MainActivity.kt
@@ -1,7 +1,7 @@
 package com.cyrilpillai.userful
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
